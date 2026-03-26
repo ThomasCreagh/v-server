5f01579c0d4be5a70996783c7e2e16cd37c73d4e
tom <1mn8wx5y@3rktz5yu0ubrq9b6.org>
2026-03-26 21:12:16 +0100

    2026-03-26 21:12:14  26.05.20260213.a82ccc3

--- config/nginx.nix
+++ config/nginx.nix
@@ -10,7 +10,7 @@ let
   mkWellKnown = data: ''
     default_type application/json;
     add_header Access-Control-Allow-Origin *;
-    return 200 "${builtins.toJSON data}";
+    return 200 '''${builtins.toJSON data}''';
   '';
 in {
   services.nginx = {
